dbcf2be848df9405a5fddba1d7ec0bb619c1d274
Joseph Claudio <atsuro4me@yahoo.com>
2018-05-01 16:18:54 -0400

    'Auto'

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,199 +11,28 @@ export default class Main extends React.Component {
       <div id="main" style={this.props.timeout ? { display: 'flex' } : { display: 'none' }}>
         {/* -------------------------------------------------------------------------- */}
         <article id="Dentures" className={`${this.props.article === 'Dentures' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{ display: 'none' }}>
-        <div>  
-        <h3 className="major">Dentures</h3>
-          <p>
-            <strong>We offer different package for dentures: </strong>
-          </p>
+          <h3 className="major">Dentures</h3>
 
           {close}
-          </div>
-        </article>
+      </article>
         {/* -------------------------------------------------------------------------- */}
         <article id="Partials" className={`${this.props.article === 'Partials' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{ display: 'none' }}>
-        <div>  
-        <h3 className="major">FRS&nbsp;Partials</h3>
-          <Img className={styles.leftImg} resolutions={this.props.imagePartial1.resolutions} />
-          <Img className={styles.rightImg} resolutions={this.props.imagePartial2.resolutions} />
-          <div className={styles.clear}>
-            <p>
-              The most important procedure is the preliminary or diagnostic cast. The cast must be surveyed and then designed for a removable partial denture. This preliminary survey and design will form a basis for restorative treatment of abutment teeth and will indicate to the technician certain procedures that may need to be completed before a final impression is made.
-            </p>
-          </div>
-
-          <Img className={styles.rightImg} resolutions={this.props.imagePartial3.resolutions} />
-          <strong className={styles.paddingTop}>We have three packages: </strong>
-          <ul className={styles.paddingBottom}>
-            <li>Premium</li>
-            <li>Custom</li>
-            <li>Economy (transitional partials)</li>
-          </ul>
-
-          <Img className={styles.rightImg} resolutions={this.props.imagePartial4.resolutions} />
-          <strong className={styles.paddingTop}>We have different choices for Partial Dentures: </strong>
-          <ul className={styles.paddingBottom}>
-            <li>Acrylic with wrought wire clasps</li>
-            <li>Flexible FRS </li>
-            <li>Cast Metal Vitallium 2000</li>
-            <li>Combo Crown &amp; Bridge </li>
-          </ul>
-          <Img className={styles.leftImg} resolutions={this.props.imagePartial5.resolutions} />
-          <Img className={styles.rightImg} resolutions={this.props.imagePartial6.resolutions} />
+          <h3 className="major">FRS&nbsp;Partials</h3>
           {close}
-          </div>
         </article>
         {/* -------------------------------------------------------------------------- */}
         <article id="CrownAndBridge" className={`${this.props.article === 'CrownAndBridge' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{ display: 'none' }}>
           <h3 className="major">Crown & Bridge</h3>
-
-          <div className={styles.clear}>
-            <p>
-              <strong>Our In-house milling system allows us to create, mill, finish, and return IPS e.max CAD crowns in 3 days, and Zirconia crowns in 5 days. </strong>
-            Our partnership with DENTSPLY allows us to offer a wide variety of services normally to difficult to obtain.
-            </p>
-          </div>
-
-
-            <p>
-              <a href="https://www.dentsplysirona.com/en-us">DENTSPLY Prident</a> is an ISO 9001
-            certified laboratory and a wholly owned subsidiary of DENTSPLY International,
-            Inc. DENTSPLY Prident guarantees that the materials used in the finished
-            medical device produced at DENTSPLY Prident are cleared by the FDA and are
-            legally distributed in the US.
-            </p>
-            <p>
-            The Turn-around Time is less than 15 days for the following:
-
-              <ul>
-                <li>PFM</li>
-                <li>Captek ™ </li>
-                <li>All-Ceramic &#xAE;</li>
-                <li>Radica </li>
-                <li>Cercon &#xAE; </li>
-              </ul>
-            </p>
-
-          <Img className={styles.leftImg} resolutions={this.props.imageCrownAndBridge1.resolutions}/>
-          <Img className={styles.rightImg} resolutions={this.props.imageCrownAndBridge3.resolutions}/>
-
           {close}
         </article>
         {/* -------------------------------------------------------------------------- */}
         <article id="Cast" className={`${this.props.article === 'Cast' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{ display: 'none' }}>
           <h3 className="major">Cast Metal Frames</h3>
-
-          <Img className={styles.leftImg} resolutions={this.props.imageCast1.resolutions}/>
-          <Img className={styles.rightImg} resolutions={this.props.imageCast2.resolutions}/>
-
-          <div className={styles.clear}>
-            <p>
-            The metal framework does not contact the gums. Thus, as the gums resorb,
-            this type of partial does not sink with them and rarely requires relines.
-            Because the teeth are altered by the dentist beforehand, there are fewer
-            limitations in the placement of clasps, and they are less likely to be seen
-            than the wrought wire clasps of the treatment partial. Modern frameworks
-            are cast from an extremely strong alloy called chrome cobalt which can be
-            cast very thin and are much less likely to break than the all plastic variety.
-            They are also much less noticeable to the tongue.
-            </p>
-          </div>
-          <ul>
-            <li>Vitallium &#xAE; 2000 Metal</li>
-            <li>Dentures come in Economy, Premium, or Custom deals</li>
-            <li>Product of Glidewell</li>
-            <li>In Vita A1 - D4 Shade</li>
-          </ul>
-          <p>
-          The largest single advantage that cast metal framework partial dentures
-          have over the newer flexible framework partials is that sore spots are almost
-          never an issue since neither the framework, nor the plastic extensions contact
-          the soft oral tissues with any force! Patients who exhibit the symptoms
-          of TMJ, or who are known bruxers are much better off with cast metal partials
-          than with flexible framework partials.
-          </p>
-
-          <Img className={styles.leftImg} resolutions={this.props.imageCast3.resolutions}/>
-          <Img className={styles.rightImg} resolutions={this.props.imageCast4.resolutions}/>
-
           {close}
         </article>
         {/* -------------------------------------------------------------------------- */}
         <article id="Other" className={`${this.props.article === 'Other' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{ display: 'none' }}>
           <h3 className="major">Other Products</h3>
-
-          <div className={styles.flexContainer}>
-
-            <div className={styles.flexItems}>
-              <Img className={styles.leftImg} resolutions={this.props.imageOther1.resolutions}/>
-              <div className={styles.clear}>
-                <p>
-                  <strong>Abutments</strong>
-                  <ul>
-                    <li>Patient-Specific abutments</li>
-                    <li>For perfect fitting, everytime</li>
-                  </ul>
-                </p>
-              </div>
-            </div>
-
-            <div className={styles.flexItems}>
-              <Img className={styles.leftImg} resolutions={this.props.imageOther2.resolutions}/>
-              <div className={styles.clear}>
-                <p>
-                  <strong>Nightguards</strong>
-                  <ul>
-                    <li>X</li>
-                    <li>X</li>
-                  </ul>
-                </p>
-              </div>
-            </div>
-
-            <div className={styles.flexItems}>
-              <Img className={styles.leftImg} resolutions={this.props.imageOther3.resolutions}/>
-              <div className={styles.clear}>
-                <p>
-                  <strong>Wax patterns</strong>
-                  <ul>
-                    <li>X</li>
-                    <li>X</li>
-                    <li>X</li>
-                  </ul>
-                </p>
-              </div>
-            </div>
-
-            <div className={styles.flexItems}>
-              <Img className={styles.leftImg} resolutions={this.props.imageOther4.resolutions}/>
-              <div className={styles.clear}>
-                <p>
-                  <strong>Study Models</strong>
-                  <ul>
-                    <li>X</li>
-                    <li>X</li>
-                  </ul>
-                </p>
-              </div>
-            </div>
-
-            <div className={styles.flexItems}>
-              <Img className={styles.leftImg} resolutions={this.props.imageOther5.resolutions}/>
-              <div className={styles.clear}>
-                <p>
-                  <strong>PFMs</strong>
-                  <ul>
-                    <li>X</li>
-                    <li>X</li>
-                    <li>X</li>
-                  </ul>
-                </p>
-              </div>
-            </div>
-
-          </div>
-
-          <p>And anything else you need, <strong>just ask!</strong></p>
 
           {close}
         </article>
@@ -211,126 +40,6 @@ export default class Main extends React.Component {
         <article id="Digital" className={`${this.props.article === 'Digital' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{ display: 'none' }}>
           <h3 className="major">Digital</h3>
 
-          <p>
-          With our digital equipment and in-house milling system, we can efficiently
-          and accurately create fully anatomical crowns and bridges as well as
-          veneers, and inlays/onlays.
-          </p>
-
-          <div className={styles.flexContainer}>
-
-            <div className={styles.flexItems}>
-              <Img className={styles.leftImg} resolutions={this.props.imageDigital1.resolutions}/>
-              <div className={styles.clear}>
-                <p>
-                  <strong>Digital Scanning</strong>
-                  <ul>
-                    <li>
-                      <p>Precise and efficient scans make your ending products accurate</p>
-                    </li>
-                  </ul>
-                </p>
-              </div>
-            </div>
-
-            <div className={styles.flexItems}>
-              <Img className={styles.leftImg} resolutions={this.props.imageDigital2.resolutions}/>
-              <div className={styles.clear}>
-                <p>
-                  <strong>Natural Esthetics</strong>
-                  <ul>
-                    <li>Beautiful and Natural look</li>
-                    <li>Designs based off surrounding dentition</li>
-                  </ul>
-                </p>
-              </div>
-            </div>
-
-            <div className={styles.flexItems}>
-              <Img className={styles.leftImg} resolutions={this.props.imageDigital3.resolutions}/>
-              <div className={styles.clear}>
-                <p>
-                  <strong>Virtual Try-In</strong>
-                  <ul>
-                    <li>
-                      <p>3D-generated Patient Pictures allows facial features to be taken into account</p>
-                    </li>
-                  </ul>
-                </p>
-              </div>
-            </div>
-
-            <div className={styles.flexItems}>
-              <Img className={styles.leftImg} resolutions={this.props.imageDigital4.resolutions}/>
-              <div className={styles.clear}>
-                <p>
-                  <strong>Virtual Articulation</strong>
-                  <ul>
-                    <li>
-                    Virtual Articulation allows us to easily determine contact surfaces
-                    </li>
-                  </ul>
-                </p>
-              </div>
-            </div>
-
-            <div className={styles.flexItems}>
-              <Img className={styles.leftImg} resolutions={this.props.imageDigital5.resolutions}/>
-              <div className={styles.clear}>
-                <p>
-                  <strong>Inlay/Onlay and Veneers</strong>
-                  <ul>
-                    <li>
-                    Anything you need, we can make!
-                    </li>
-                  </ul>
-                </p>
-              </div>
-            </div>
-
-            <div className={styles.flexItems}>
-              <Img className={styles.leftImg} resolutions={this.props.imageDigital6.resolutions}/>
-              <div className={styles.clear}>
-                <p>
-                  <strong>Crown and Bridge</strong>
-                  <ul>
-                    <li>Full Contour or copings</li>
-                    <li>
-                    IPS e.max, inCoris TZI/ZI (BruxZir), Lava, VITABLOCS, and many more!
-                    </li>
-                  </ul>
-                </p>
-              </div>
-            </div>
-
-            <div className={styles.flexItems}>
-              <Img className={styles.leftImg} resolutions={this.props.imageDigital7.resolutions}/>
-              <div className={styles.clear}>
-                <p>
-                  <strong>In-House Milling</strong>
-                  <ul>
-                    <li>
-                    We mill it all right here so you get your case back ASAP!
-                    </li>
-                  </ul>
-                </p>
-              </div>
-            </div>
-
-            <div className={styles.flexItems}>
-              <Img className={styles.leftImg} resolutions={this.props.imageDigital8.resolutions}/>
-              <div className={styles.clear}>
-                <p>
-                  <strong>CEREC Connect</strong>
-                  <ul>
-                    <li>
-                    Finally go model free and send us digital scans for the best results!
-                    </li>
-                  </ul>
-                </p>
-              </div>
-            </div>
-          </div>
 
           {close}
         </article>
