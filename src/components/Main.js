f5be9b37abb15616f1ec7097a1284a4c8b45efed
Joseph Claudio <atsuro4me@yahoo.com>
2018-04-30 23:01:09 -0400

    'Auto'

--- src/components/Main.js
+++ src/components/Main.js
@@ -98,7 +98,8 @@ export default class Main extends React.Component {
         </article>
 
         <article id="Partials" className={`${this.props.article === 'Partials' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{ display: 'none' }}>
-          <h3 className="major">FRS&nbsp;Partials</h3>
+        <div>  
+        <h3 className="major">FRS&nbsp;Partials</h3>
           <Img className={styles.leftImg} resolutions={this.props.imagePartial1.resolutions} />
           <Img className={styles.rightImg} resolutions={this.props.imagePartial2.resolutions} />
           <div className={styles.clear}>
@@ -126,41 +127,9 @@ export default class Main extends React.Component {
           <Img className={styles.leftImg} resolutions={this.props.imagePartial5.resolutions} />
           <Img className={styles.rightImg} resolutions={this.props.imagePartial6.resolutions} />
           {close}
-        </article>
-
-        {/* -------------------------------------------------------------------------- */}
-        <article id="Partials" className={`${this.props.article === 'Partials' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{ display: 'none' }}>
-          <h3 className="major">FRS&nbsp;Partials</h3>
-          <Img className={styles.leftImg} resolutions={this.props.imagePartial1.resolutions} />
-          <Img className={styles.rightImg} resolutions={this.props.imagePartial2.resolutions} />
-          <div className={styles.clear}>
-            <p>
-              The most important procedure is the preliminary or diagnostic cast. The cast must be surveyed and then designed for a removable partial denture. This preliminary survey and design will form a basis for restorative treatment of abutment teeth and will indicate to the technician certain procedures that may need to be completed before a final impression is made.
-            </p>
           </div>
-
-          <Img className={styles.rightImg} resolutions={this.props.imagePartial3.resolutions} />
-          <strong className={styles.paddingTop}>We have three packages: </strong>
-          <ul className={styles.paddingBottom}>
-            <li>Premium</li>
-            <li>Custom</li>
-            <li>Economy (transitional partials)</li>
-          </ul>
-
-          <Img className={styles.rightImg} resolutions={this.props.imagePartial4.resolutions} />
-          <strong className={styles.paddingTop}>We have different choices for Partial Dentures: </strong>
-          <ul className={styles.paddingBottom}>
-            <li>Acrylic with wrought wire clasps</li>
-            <li>Flexible FRS </li>
-            <li>Cast Metal Vitallium 2000</li>
-            <li>Combo Crown &amp; Bridge </li>
-          </ul>
-          <Img className={styles.leftImg} resolutions={this.props.imagePartial5.resolutions} />
-          <Img className={styles.rightImg} resolutions={this.props.imagePartial6.resolutions} />
-          {close}
         </article>
-        {/* -------------------------------------------------------------------------- */}
-
+        
       </div>
     )
   }
