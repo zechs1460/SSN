f1103088198bf2ad13998cc6aaab3e189037a660
Joseph Claudio <atsuro4me@yahoo.com>
2018-05-04 14:52:05 -0400

    'Auto'

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -194,7 +194,7 @@ query PageQuery {
     }
   }
   background: imageSharp(id: { regex: "/bg.jpg/" }) {
-    sizes(maxWidth: 1920, quality: 100) {
+    sizes(maxWidth: 1920, quality: 90) {
       ...GatsbyImageSharpSizes
     }
   }
